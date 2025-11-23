56c7f922cec3fe84440cfc2f83663f8901d66661
axreldable <aleksei.starikov.ax@gmail.com>
2025-11-23 15:29:02 +0100

    ci: Fix pre-commit

--- python/ax_lang/interpreter/ax_lang.py
+++ python/ax_lang/interpreter/ax_lang.py
@@ -1,8 +1,9 @@
 from numbers import Number
+from typing import Any
 
 
 class AxLang:
-    def eval(self, expr):
+    def eval(self, expr: str) -> Any:  # noqa: A003
         if isinstance(expr, Number):
             return expr
 
