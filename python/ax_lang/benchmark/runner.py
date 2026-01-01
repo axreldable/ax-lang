2ea4faac691d6296890ba324129814e5a3bf9139
axreldable <aleksei.starikov.ax@gmail.com>
2026-01-01 18:34:23 +0100

    Add todo

--- python/ax_lang/benchmark/runner.py
+++ python/ax_lang/benchmark/runner.py
@@ -4,6 +4,10 @@ from ax_lang.benchmark.benchmark import BenchmarkRunner
 if __name__ == "__main__":
     tests = ["factorial", "fibonacci", "higher_order", "simple", "switch"]
 
+    # todo:
+    #     finish visual
+    #     integrate to ci
+    #     have it as a part of docs
     runner = BenchmarkRunner(["python"], tests)
     # runner = BenchmarkRunner(["axlang"], tests)
     results = runner.run()
